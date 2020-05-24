10629b952f31dcb8499297cf8ae5fcc6dea21cac
Cleve <mauricio.cleveland@gmail.com>
2020-05-24 11:14:07 -0400

    Components added

--- src/core/DataBase.java
+++ src/core/DataBase.java
@@ -1,7 +1,55 @@
 package core;
 
+import org.lmdbjava.CursorIterator;
+import org.lmdbjava.Dbi;
+import org.lmdbjava.Env;
+import org.lmdbjava.Txn;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
 public class DataBase {
-    public void DataBase(String db_path) {
-        
+    private String pathLmdb = null;
+    private Dbi<ByteBuffer> db;
+    private Env<ByteBuffer> env;
+
+    public DataBase(String dbPath) {
+        pathLmdb = dbPath;
+        File dbDirectory = new File(dbPath);
+        env = Env.create()
+                .open(dbDirectory);
+
+        for (byte[] obj:env.getDbiNames()) {
+            System.out.println(new String(obj));
+        }
+        System.out.println(env.getDbiNames());
+        System.out.println(env.stat());
+
+
     }
+
+    public String GetDbNames() {
+        if (db == null) {
+            return "";
+        }
+        return "String.valueOf(.getName())";
+    }
+
+    public ArrayList<DataStructure> GetData() {
+        ArrayList<DataStructure> results = null;
+        try(Txn<ByteBuffer> txn = env.txnRead()) {
+            CursorIterator<ByteBuffer> cursor = db.iterate(txn);
+
+            for(CursorIterator.KeyVal<ByteBuffer> kv : cursor.iterable()) {
+                ByteBuffer value = kv.val();
+                byte[] bytes = new byte[value.remaining()];
+                value.get(bytes);
+                System.out.println(bytes);
+            }
+        }
+        return results;
+    }
+
+
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,22 @@
 package sample;
 
+import core.DataBase;
 import javafx.event.ActionEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class Controller {
     public void sayHelloWorld(ActionEvent actionEvent) {
         System.out.println("Click");
+        DataBase dataBase = new DataBase("/home/mauricio/workspace/lab/python/varidb/app/storage/volume_st.db");
+
     }
+
+    public void openDatabase(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select File");
+        Stage stage = new Stage();
+        fileChooser.showOpenDialog(stage);
+    }
+
 }
