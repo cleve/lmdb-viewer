8458cc8424458c7d94343f802b4f1f14f4f286dc
Cleve <mauricio.cleveland@gmail.com>
2020-05-31 20:21:51 -0400

    Adding Thread support

--- src/core/ThreadHelper.java
+++ src/core/ThreadHelper.java
@@ -1,6 +1,19 @@
 package core;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadHelper {
+    private final ExecutorService mExecutorService = Executors.newFixedThreadPool(1);
+
     public ThreadHelper() {
+    }
+
+    public void sendTask(Runnable runnable) {
+        try {
+            mExecutorService.submit(runnable);
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
     }
 }
